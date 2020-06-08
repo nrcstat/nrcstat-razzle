f629381329ddffa9c4826d99439b36d2d88049dc
Eric Bolikowski <eric@binarylights.com>
2020-06-08 11:39:57 +0200

    remove dead code

--- public/widgetLib.js
+++ public/widgetLib.js
@@ -42,21 +42,5 @@
           document.querySelector('body').appendChild(linkEl)
         })
       })
-
-    /*
-    // TODO: Check proper loader libraries for better techniques here
-    const scriptElement = document.createElement('script')
-    scriptElement.src = jsPath
-    */
-
-    /*
-    const linkElement = document.createElement('link')
-    linkElement.href = cssPath
-    linkElement.rel = 'stylesheet'
-    linkElement.type = 'text/css'
-    */
-
-    // document.querySelector('body').appendChild(scriptElement)
-    // document.querySelector('body').appendChild(linkElement)
   }
 })()
