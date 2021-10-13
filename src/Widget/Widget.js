62f1022f1de4ddcaf1d14145afc15782f1add6af
Eric Bolikowski <eric@binarylights.com>
2021-10-13 09:45:22 +0200

    fix: rerender bug in visualization builder

--- src/Widget/Widget.js
+++ src/Widget/Widget.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './Widget.scss'
 import { isServer } from '../util/utils'
 import loadable from '@loadable/component'
@@ -11,14 +10,32 @@ import Line from './Line/Line'
 import Donut from './Donut/Donut'
 import Pie from './Pie/Pie'
 */
-const GlobalMap = loadable(() => import(/* webpackChunkName: "GlobalMap" */ './GlobalMap/GlobalMap'))
-const Line = loadable(() => import(/* webpackChunkName: "Line" */ './Line/Line'))
-const Donut = loadable(() => import(/* webpackChunkName: "Donut" */ './Donut/Donut'))
-const StaticTable = loadable(() => import(/* webpackChunkName: "StaticTable" */ './StaticTable/StaticTable'))
-const CountryDashboard = loadable(() => import(/* webpackChunkName: "CountryDashboard" */ './CountryDashboard/CountryDashboard'))
-const CustomTable = loadable(() => import(/* webpackChunkName: "CustomTable" */ './CustomTable/CustomTable'))
-const Pictogram = loadable(() => import(/* webpackChunkName: "Pictogram" */ './Pictogram/Pictogram'))
-const Timeline = loadable(() => import(/* webpackChunkName: "Timeline" */ './Timeline/Timeline'))
+const GlobalMap = loadable(() =>
+  import(/* webpackChunkName: "GlobalMap" */ './GlobalMap/GlobalMap')
+)
+const Line = loadable(() =>
+  import(/* webpackChunkName: "Line" */ './Line/Line')
+)
+const Donut = loadable(() =>
+  import(/* webpackChunkName: "Donut" */ './Donut/Donut')
+)
+const StaticTable = loadable(() =>
+  import(/* webpackChunkName: "StaticTable" */ './StaticTable/StaticTable')
+)
+const CountryDashboard = loadable(() =>
+  import(
+    /* webpackChunkName: "CountryDashboard" */ './CountryDashboard/CountryDashboard'
+  )
+)
+const CustomTable = loadable(() =>
+  import(/* webpackChunkName: "CustomTable" */ './CustomTable/CustomTable')
+)
+const Pictogram = loadable(() =>
+  import(/* webpackChunkName: "Pictogram" */ './Pictogram/Pictogram')
+)
+const Timeline = loadable(() =>
+  import(/* webpackChunkName: "Timeline" */ './Timeline/Timeline')
+)
 
 const widgetMap = {
   GlobalMap: GlobalMap,
@@ -28,21 +45,21 @@ const widgetMap = {
   CountryDashboard: CountryDashboard,
   CustomTable: CustomTable,
   Pictogram: Pictogram,
-  Timeline: Timeline
+  Timeline: Timeline,
 }
 
 export const WidgetParamsContext = React.createContext()
 
-function Widget (props) {
+function Widget(props) {
   const { type, locale } = props
   const SpecificWidget = widgetMap[type]
   const FixedLocaleContext = buildFixedLocaleContext(locale || 'nb-NO')
-  const containerRef = React.useRef()
-  const onReady = ref => { containerRef.current = ref }
+  // const containerRef = React.useRef()
+  // const onReady = ref => { containerRef.current = ref }
   return (
     <FixedLocaleContext>
-      <WidgetParamsContext.Provider value={{ ...props, containerRef }}>
-        <div className='nrcstat__rootwidget' ref={onReady}>
+      <WidgetParamsContext.Provider value={{ ...props }}>
+        <div className="nrcstat__rootwidget">
           <SpecificWidget />
         </div>
       </WidgetParamsContext.Provider>
--- src/client.js
+++ src/client.js
@@ -6,8 +6,26 @@ import { loadableReady } from '@loadable/component'
 
 import './services/i18n.js'
 
+import { isClient } from './util/utils'
+
+// There's a very good reason we're doing this. In the special case of
+// VB (Visualization Builder) using me/Razzle to render widgets, there's
+// a rerender happening after every change to the widget object being
+// built. We need to provide "this" copy of ReactDOM to VB via window
+// to VB, so that VB cnn properly <Widget> so that "this" ReactDOM does
+// not compete with the "next" ReactDOM provided in the next render.
+//
+// See nrcstat-monorepo:libs/widget-social-media-sharing/src/lib/index.ts:13
+// for another in-depth comment
+if (isClient()) {
+  window.razzleReactDOM = ReactDOM
+}
+
 loadableReady(() => {
-  window.nrcStatDrawWidgetQueue.forEach(params => {
-    ReactDOM.render(<Widget {...params} />, document.getElementById(params.targetSelector.replace(/#/g, '')))
+  window.nrcStatDrawWidgetQueue.forEach((params) => {
+    ReactDOM.render(
+      <Widget {...params} />,
+      document.getElementById(params.targetSelector.replace(/#/g, ''))
+    )
   })
 })
