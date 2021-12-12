715b62dc08536f4b282dfb1921dd1888dbbb855a
Eric Bolikowski <eric@binarylights.com>
2021-12-12 19:42:02 +0100

    feat(timeline): complete expand/collapse btn

--- src/Widget/Timeline/Timeline.js
+++ src/Widget/Timeline/Timeline.js
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useLayoutEffect, useRef, useState } from 'react'
 
 import { isMobileDevice } from '@/util/widgetHelpers.js'
 import { isServer } from '@/util/utils'
 import { WidgetParamsContext } from '../Widget'
 
+import { FixedLocaleContext } from '../../services/i18n'
+
 import c from './Timeline.module.scss'
 
 function Timeline() {
   if (isServer()) return null
-
-  console.log(c)
 
   const isMobile = isMobileDevice()
 
@@ -22,21 +22,81 @@ function Timeline() {
       className={`${c.container} ${isMobile ? c.mobile : c.desktop}`}
       ref={findElementEpiServerAncestorResetHeight}
     >
-      {entries.map(({ title, subtitle, body }, key) => (
-        <div className={c['timeline-entry']} key={key}>
-          <div className={c['timeline-marker']} />
-          <div className={c['timeline-content-line']} />
-          <div className={c['timeline-content']}>
-            <span className={c['timeline-entry-title']}>{title}</span>
-            <span className={c['timeline-entry-subtitle']}>{subtitle}</span>
-            <span className={c['timeline-entry-body']}>
-              {body}
-              <div className={c['fade-out-last-line']} />
-              {/* <button className={c['past-fade-out-expand-button']}>expand</button> */}
-            </span>
-          </div>
-        </div>
+      {entries.map((entry, key) => (
+        <TimelineEntry entry={entry} key={key} />
       ))}
+    </div>
+  )
+}
+
+function TimelineEntry({ entry }) {
+  const [accordionOpen, setAccordionOpen] = useState(false)
+  const accordionBodyRef = useRef(null)
+  const accordionButtonRef = useRef(null)
+  const accordionFaderRef = useRef(null)
+
+  useLayoutEffect(() => {
+    const accordionBodyHeight = accordionBodyRef.current.offsetHeight
+
+    if (accordionBodyHeight >= 160) {
+      accordionButtonRef.current.style.setProperty('display', 'inline-block')
+    }
+  }, [])
+
+  function handleCollapseExpandBtnClick() {
+    if (accordionOpen) closeAccordion()
+    else openAccordion()
+  }
+
+  function openAccordion() {
+    accordionBodyRef.current.style.setProperty('max-height', 'none')
+    accordionFaderRef.current.style.setProperty('visibility', 'hidden')
+    setAccordionOpen(true)
+  }
+
+  function closeAccordion() {
+    accordionBodyRef.current.style.setProperty('max-height', '165px')
+    accordionFaderRef.current.style.setProperty('visibility', 'visible')
+    setAccordionOpen(false)
+  }
+
+  const { getNsFixedT } = useContext(FixedLocaleContext)
+  const t = getNsFixedT(['Widget.UserCreated.Timeline'])
+
+  return (
+    <div className={c['timeline-entry']}>
+      <div className={c['timeline-marker']} />
+      <div className={c['timeline-content-line']} />
+      <div className={c['timeline-content']}>
+        <span className={c['timeline-entry-title']}>{entry.title}</span>
+        <span className={c['timeline-entry-subtitle']}>{entry.subtitle}</span>
+        <span className={c['timeline-entry-accordion']}>
+          <span
+            className={c['timeline-entry-accordion-body']}
+            ref={(ref) => (accordionBodyRef.current = ref)}
+          >
+            {entry.body}
+            <div
+              className={c['fade-out-last-line']}
+              ref={(ref) => (accordionFaderRef.current = ref)}
+            />
+          </span>
+          <button
+            className={c['collapse-expand-button']}
+            ref={(ref) => (accordionButtonRef.current = ref)}
+            onClick={handleCollapseExpandBtnClick}
+          >
+            {!accordionOpen ? (
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: t('expandCollapseButton.readMore'),
+                }}
+              />
+            ) : null}
+            {accordionOpen ? <>▲</> : null}
+          </button>
+        </span>
+      </div>
     </div>
   )
 }
