d105aa23ccf6a9dc64a791d840aff91ecebb1f47
Eric Bolikowski <eric@binarylights.com>
2022-06-01 12:13:27 +0200

    fix: caching problem

--- src/server.js
+++ src/server.js
@@ -83,7 +83,10 @@ server
         // Special case: if the widget ID is widget-wizard, it comes from the old
         // widget wizard or the new widget builder. Either way, there is no point
         // in caching data for these as they're not saved widgets.
-        if (dataCache[widget.widgetId] && widget.widgetId !== 'widget-wizard') {
+        if (
+          dataCache[widget.widgetId] &&
+          !/widget-wizard/.test(widget.widgetId)
+        ) {
           widget.preloadedWidgetData = dataCache[widget.widgetId]
         } else {
           const data = await dataLoader(widget)
